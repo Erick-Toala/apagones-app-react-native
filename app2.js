950419f30248ee2a54ee17b80a15a7920a831122
Erick-Toala <toalaerick56@gmail.com>
2024-11-20 13:06:15 -0500

    Inicialización del proyecto React Native

--- app2.js
+++ app2.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, FlatList, Alert, TouchableOpacity, Modal, Linking, Switch } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { AppState, View, Text, TextInput, Button, StyleSheet, FlatList, Alert, TouchableOpacity, Modal, Linking, Switch } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as Notifications from 'expo-notifications';
 
+// import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+// const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : 'ca-app-pub-1892277589295572/1580425616';
+
 export default function App() {
   const [notificationsEnabled, setNotificationsEnabled] = useState(false);
   const [numero, setNumero] = useState('');
-  const [resultados, setResultados] = useState([]);
-  const [modalVisible, setModalVisible] = useState({ contact: false, settings: false });
+  const [resultados, setResultados] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
 
-  const toggleModal = useCallback((type) => {
-    setModalVisible((prev) => ({ ...prev, [type]: !prev[type] }));
-  }, []);
 
+  // Cambia el estado y configura AsyncStorage
   const toggleNotifications = async () => {
     const newStatus = !notificationsEnabled;
     setNotificationsEnabled(newStatus);
     await AsyncStorage.setItem('notificationsEnabled', JSON.stringify(newStatus));
 
     if (newStatus) {
+      // Registra la tarea para ejecutar cada 5 minutos
       Notifications.setNotificationHandler({
         handleNotification: async () => ({
           shouldShowAlert: true,
@@ -28,64 +32,114 @@ export default function App() {
         }),
       });
     } else {
+      // Cancela todas las notificaciones cuando se desactiven
       await Notifications.cancelAllScheduledNotificationsAsync();
     }
   };
 
-  const loadSettings = async () => {
-    const numeroGuardado = await AsyncStorage.getItem('numero');
-    const resultadosGuardados = await AsyncStorage.getItem('resultados');
-    const storedSetting = await AsyncStorage.getItem('notificationsEnabled');
+  const abrirModalContacto = () => {
+    setModalVisible(true);
+  };
 
-    setNumero(numeroGuardado || '');
-    setResultados(resultadosGuardados ? JSON.parse(resultadosGuardados) : []);
-    setNotificationsEnabled(storedSetting === 'true');
+  const cerrarModalContacto = () => {
+    setModalVisible(false);
+  };
 
-    if (numeroGuardado) consultarCortes(numeroGuardado);
+  const abrirModalConfiguracion = () => {
+    setModalVisible2(true);
+  };
+
+  const cerrarModalConfiguracion = () => {
+    setModalVisible2(false);
   };
 
   useEffect(() => {
-    loadSettings();
+    const cargarDatos = async () => {
+      const numeroGuardado = await AsyncStorage.getItem('numero');
+      const resultadosGuardados = await AsyncStorage.getItem('resultados');
+
+      if (numeroGuardado) {
+        setNumero(numeroGuardado);
+        await consultarCortes(numeroGuardado); // Realizar la consulta automáticamente si hay un número guardado válido
+      }
+      if (resultadosGuardados) {
+        setResultados(JSON.parse(resultadosGuardados));
+      }
+    };
+    cargarDatos();
+
+    const loadNotificationSetting = async () => {
+      const storedSetting = await AsyncStorage.getItem('notificationsEnabled');
+      if (storedSetting !== null) {
+        setNotificationsEnabled(JSON.parse(storedSetting));
+      }
+    };
+    loadNotificationSetting();
   }, []);
 
-  const programarNotificacionCorte = async (detalle) => {
-    const corteFechaHora = new Date(detalle.fechaHoraCorte);
-    const notificacionFechaHora = new Date(corteFechaHora.getTime() - 15 * 60 * 1000);
-    const ahora = new Date();
 
+  const programarNotificacionCorte = async (detalle) => {
+    // Calcula la hora de la notificación (15 minutos antes del corte)
+    const corteFechaHora = new Date(detalle.fechaHoraCorte);
+    const notificacionFechaHora = new Date(corteFechaHora.getTime() - 15 * 60 * 1000); // 15 minutos antes
+  
+    const ahora = new Date();
     if (notificacionFechaHora > ahora) {
       await Notifications.scheduleNotificationAsync({
-        content: { title: 'Corte de luz próximo', body: `Desde: ${detalle.horaDesde} hasta: ${detalle.horaHasta}.` },
-        trigger: { date: notificacionFechaHora },
+        content: {
+          title: 'Corte de luz próximo',
+          body: `Desde: ${detalle.horaDesde} hasta: ${detalle.horaHasta}.`,
+        },
+        trigger: {
+          date: notificacionFechaHora,
+        },
       });
     }
   };
 
-  const consultarCortes = async (numeroConsulta = numero) => {
-    if (!numeroConsulta) return Alert.alert("Error", "Por favor, ingrese un número válido.");
-
+  const consultarCortes = async (numeroParam) => {
+    const numeroConsulta = numeroParam || numero; // Usar númeroParam si está disponible, o el valor de numero del estado
+    if (!numeroConsulta) {
+      Alert.alert("Error", "Por favor, ingrese un número válido.");
+      return;
+    }
     try {
-      await AsyncStorage.setItem('numero', numeroConsulta);
-      const response = await fetch(`https://api.cnelep.gob.ec/servicios-linea/v1/notificaciones/consultar/${numeroConsulta}/IDENTIFICACION`);
+      await AsyncStorage.setItem('numero', numeroConsulta); // Guardar el número aquí
+      const criterio = 'IDENTIFICACION';
+      const response = await fetch(`https://api.cnelep.gob.ec/servicios-linea/v1/notificaciones/consultar/${numeroConsulta}/${criterio}`);
       const data = await response.json();
 
       if (data.resp === 'OK') {
         setResultados(data.notificaciones || []);
-        await AsyncStorage.setItem('resultados', JSON.stringify(data.notificaciones || []));
+        await AsyncStorage.setItem('resultados', JSON.stringify(data.notificaciones || [])); // Guardar los resultados aquí
+
+        // Cancela las notificaciones programadas anteriores
         await Notifications.cancelAllScheduledNotificationsAsync();
 
-        data.notificaciones.forEach((notificacion) =>
-          notificacion.detallePlanificacion.forEach(programarNotificacionCorte)
-        );
+        // Programa las notificaciones para los próximos cortes
+        data.notificaciones.forEach((notificacion) => {
+          notificacion.detallePlanificacion.forEach((detalle) => {
+            programarNotificacionCorte(detalle);
+          });
+        });
       } else {
         Alert.alert("Error", data.mensajeError || "No se encontraron resultados.");
         setResultados([]);
-        await AsyncStorage.removeItem('resultados');
+        await AsyncStorage.removeItem('resultados'); // Limpiar resultados si no hay datos
       }
     } catch (error) {
       console.error('Error al consultar la API:', error);
       Alert.alert("Error", "No se pudo conectar con la API.");
     }
+  };
+
+  const formatHora = (hora) => {
+    const [hours, minutes] = hora.split(':');
+    return `${hours.padStart(2, '0')}:${minutes}`;
+  };
+
+  const mostrarInfo = () => {
+    Alert.alert("Información", "Para consultar los cortes en su sector debe ingresar el número de cédula de un cliente CNEL.");
   };
 
   const renderItem = ({ item }) => (
@@ -102,17 +156,20 @@ export default function App() {
         data={item.detallePlanificacion}
         keyExtractor={(detalle) => detalle.fechaHoraCorte}
         renderItem={({ item: detalle }) => {
-          const pasado = new Date(detalle.fechaHoraCorte) < new Date();
+          const corteFechaHora = new Date(detalle.fechaHoraCorte);
+          const ahora = new Date();
+          const pasado = corteFechaHora < ahora;
+
           return (
             <View style={[styles.detalle, pasado && styles.detallePasado]}>
               <Text style={[styles.fecha, pasado && styles.fechaPasado]}>{detalle.fechaCorte}</Text>
               <View style={styles.horaContainer}>
                 <View style={[styles.horaBox, pasado && styles.horaBoxPasado]}>
-                  <Text style={[styles.horaTexto, pasado && styles.horaTextoPasado]}>Desde: {detalle.horaDesde}</Text>
+                  <Text style={[styles.horaTexto, pasado && styles.horaTextoPasado]}>Desde: {formatHora(detalle.horaDesde)}</Text>
                 </View>
                 <Text style={styles.flecha}>→</Text>
                 <View style={[styles.horaBox, pasado && styles.horaBoxPasado]}>
-                  <Text style={[styles.horaTexto, pasado && styles.horaTextoPasado]}>Hasta: {detalle.horaHasta}</Text>
+                  <Text style={[styles.horaTexto, pasado && styles.horaTextoPasado]}>Hasta: {formatHora(detalle.horaHasta)}</Text>
                 </View>
               </View>
             </View>
@@ -126,7 +183,7 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Consulta de Cortes de Luz</Text>
-        <TouchableOpacity onPress={() => toggleModal('settings')}>
+        <TouchableOpacity onPress={() => setModalVisible2(true)}>
           <Icon name="settings-outline" size={24} color="#007bff" />
         </TouchableOpacity>
       </View>
@@ -135,66 +192,94 @@ export default function App() {
           style={styles.input}
           placeholder="Ingrese un número de cédula"
           value={numero}
-          onChangeText={setNumero}
+          onChangeText={(text) => setNumero(text)}
           keyboardType="numeric"
         />
-        <TouchableOpacity onPress={() => Alert.alert("Información", "Para consultar los cortes en su sector debe ingresar el número de cédula de un cliente CNEL.")}>
+        <TouchableOpacity onPress={mostrarInfo}>
           <Icon name="information-circle-outline" size={24} color="#007bff" style={styles.icon} />
         </TouchableOpacity>
       </View>
       <Button title="Consultar" onPress={() => consultarCortes()} />
-      <FlatList data={resultados} keyExtractor={(item, index) => index.toString()} renderItem={renderItem} />
-
+      <FlatList
+        data={resultados}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
+      {/* <View>
+        <BannerAd
+          unitId={adUnitId}
+          size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          requestOptions={{
+            networkExtras: {
+              collapsible: 'bottom',
+            },
+          }}
+        />
+      </View> */}
       <View style={styles.footer}>
         <Text style={styles.footerText}>Desarrollado por </Text>
-        <TouchableOpacity onPress={() => toggleModal('contact')}>
+        <TouchableOpacity onPress={abrirModalContacto}>
           <Text style={[styles.footerText, styles.footerLink]}>Erick Toala</Text>
         </TouchableOpacity>
         <Text style={styles.footerText}> ©2024</Text>
       </View>
-
-      <Modal visible={modalVisible.contact} transparent onRequestClose={() => toggleModal('contact')}>
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={cerrarModalContacto}
+      >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Información de Contacto</Text>
             <Text style={styles.modalText}>Erick Alexander Toala Intriago</Text>
             <Text style={styles.modalText}>Email: toalaerick56@gmail.com</Text>
+            {/* Redes Sociales */}
             <View style={styles.socialIcons}>
               <TouchableOpacity onPress={() => Linking.openURL("https://github.com/Erick-Toala")}>
-                <Icon name="logo-github" size={34} color="#333" />
+                <Icon name="logo-github" size={34} color="#333" style={styles.icon} />
               </TouchableOpacity>
               <TouchableOpacity onPress={() => Linking.openURL("https://www.instagram.com/toalaerick56")}>
-                <Icon name="logo-instagram" size={34} color="#3b5998" />
+                <Icon name="logo-instagram" size={34} color="#3b5998" style={styles.icon} />
               </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={() => toggleModal('contact')} style={styles.closeButton}>
+
+            <TouchableOpacity onPress={cerrarModalContacto} style={styles.closeButton}>
               <Text style={styles.closeButtonText}>Cerrar</Text>
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
-
-      <Modal visible={modalVisible.settings} transparent onRequestClose={() => toggleModal('settings')}>
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={modalVisible2}
+        onRequestClose={cerrarModalConfiguracion}
+      >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Notificaciones</Text>
             <View style={styles.switchContainer}>
               <Text>Activar Notificaciones</Text>
-              <Switch value={notificationsEnabled} onValueChange={toggleNotifications} />
+              <Switch
+                value={notificationsEnabled}
+                onValueChange={toggleNotifications}
+              />
             </View>
-            <TouchableOpacity onPress={() => toggleModal('settings')} style={styles.closeButton}>
+            <TouchableOpacity onPress={cerrarModalConfiguracion} style={styles.closeButton}>
               <Text style={styles.closeButtonText}>Cerrar</Text>
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, backgroundColor: '#f5f5f5', paddingTop: 20 },
-  title: { fontSize: 24, textAlign: 'center'},
+  title: { fontSize: 24, textAlign: 'center', marginBottom: 20 },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
